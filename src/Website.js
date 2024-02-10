f2bf001afb24fff478acce660ae4b1696c485ce3
vineeth <kondeti.vineeth@gmail.com>
2024-02-10 12:59:49 +0530

    chatbot changes

--- src/Website.js
+++ src/Website.js
@@ -3,99 +3,66 @@ import "./nav.css";
 import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 import Home from "./home";
 import About from "./about";
-import { ToastContainer, toast } from "react-toastify";
-
-import LoadingAnimation from "./loadinganimation";
-import { useLocation } from "react-router-dom";
 import Blogs from "./blogs";
+import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { jwtDecode } from 'jwt-decode';
 
 function Navbar() {
   const [showMobileMenu, setShowMobileMenu] = useState(false);
-  const location = useLocation();
-  const shouldShrink = location.pathname !== "/";
-
-  function handleHamburgerClick() {
-    setShowMobileMenu(!showMobileMenu);
-  }
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userData, setUserData] = useState(null);
 
   useEffect(() => {
-    function handleScroll() {
-      const navbar = document.querySelector(".navbar");
-      if (navbar) {
-        if (window.scrollY > 0) {
-          navbar.classList.add("shrink");
-        } else {
-          navbar.classList.remove("shrink");
-        }
-      }
+    const token = localStorage.getItem('jwt');
+    if (token) {
+      setIsLoggedIn(true);
+      const decodedData = jwtDecode(token);
+      setUserData(decodedData);
     }
-
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
   }, []);
 
-  const showToastMessage = () => {
-    toast.success("Thankyou Visit Again!", {
-      position: toast.POSITION.TOP_RIGHT,
-    });}
+  const handleHamburgerClick = () => {
+    setShowMobileMenu(!showMobileMenu);
+  };
 
   const logout = () => {
     localStorage.removeItem('jwt');
-    
-    showToastMessage();
-    setTimeout(() => {
-      window.location.reload();
-    }, 4000); 
+    setIsLoggedIn(false);
+    showToastMessage("Logout successful!");
   };
-  const decode = localStorage.getItem('jwt')
-  const data = jwtDecode(decode)
+
+  const showToastMessage = (message) => {
+    toast.success(message, {
+      position: toast.POSITION.TOP_RIGHT,
+    });
+  };
 
   return (
-    <nav className={`navbar ${shouldShrink ? "shrink" : ""}`}>
-      <a href="#" className="logo">
-        Team-14
-      </a>
-      <div className={`nav-links ${showMobileMenu ? "mobile-menu" : ""}`}>
+    <nav className={`navbar ${showMobileMenu ? "mobile-menu" : ""}`}>
+      <a href="/" className="logo">Team-14</a>
+      <div className="nav-links">
         <ul className="nav-menu">
-          <li className="active">
-            <NavLink
-              to="/"
-              style={({ isActive }) => ({
-                color: isActive ? "greenyellow" : "white",
-              })}
-            >
-              Home
-            </NavLink>
-          </li>
-          
           <li>
-            <NavLink
-              to="/blogs"
-              style={({ isActive }) => ({
-                color: isActive ? "greenyellow" : "white",
-              })}
-            >
-              Blogs <i className="fab fa-blogger-b"></i>{" "}
-            </NavLink>
-          </li>         
-          <li><img className="profilepic" src={data.picture} alt="" />
-            <NavLink
-              to="/devteam"
-               style={({ isActive }) => ({
-                color: isActive ? "greenyellow" : "white",
-              })}
-            >
-              {" "}
-              My profile
-            </NavLink>
+            <NavLink to="/" activeClassName="activeLink">Home</NavLink>
           </li>
-          <button onClick={logout} style={{borderRadius:"10px",borderColor:"rgba(0, 0, 0, 0.5)"}}> logout</button>
-        
-          </ul>
+          <li>
+            <NavLink to="/blogs" activeClassName="activeLink">Blogs <i className="fab fa-blogger-b"></i></NavLink>
+          </li>
+          {isLoggedIn && (
+            <>
+              <li>
+                <NavLink to="/devteam" activeClassName="activeLink">
+                  <img className="profilepic" src={userData?.picture} alt="" />
+                  My profile
+                </NavLink>
+              </li>
+              <li>
+                <button onClick={logout} style={{ borderRadius: "10px", borderColor: "rgba(0, 0, 0, 0.5)" }}>Logout</button>
+              </li>
+            </>
+          )}
+        </ul>
       </div>
       <i className="menu-hamburger fa fa-bars" onClick={handleHamburgerClick}></i>
     </nav>
@@ -107,32 +74,17 @@ function Header() {
 }
 
 export default function Website() {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsLoading(false); // simulate a 2-second loading time
-    }, 0);
-    return () => clearTimeout(timer);
-  }, []);
-
-  
-
   return (
     <>
-      {isLoading ? (
-        <LoadingAnimation />
-      ) : (
-        <BrowserRouter>
-          <Navbar />
-          <Header />
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/devteam" element={<About />} />
-            <Route exact path="/blogs" element={<Blogs />} />
-          </Routes>
-        </BrowserRouter>
-      )}
+      <BrowserRouter>
+        <Navbar />
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/devteam" element={<About />} />
+          <Route exact path="/blogs" element={<Blogs />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/chatbot.js
+++ src/chatbot.js
@@ -1,25 +1,41 @@
 import React, { Component } from "react";
 
 class KommunicateChat extends Component {
-   constructor(props){
-       super(props);
-   } 
-componentDidMount(){
-    (function(d, m){
-        var kommunicateSettings = {"appId":"31fd68ece1d068a7f44f69dc536b1fb3a","popupWidget":true,"automaticChatOpenOnNavigation":true};
-        var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
-        s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
-        var h = document.getElementsByTagName("head")[0]; 
-        h.appendChild(s);
-        window.kommunicate = m; m._globals = kommunicateSettings;
-      })(document, window.kommunicate || {});
+    componentDidMount() {
+        // Check if Kommunicate is already loaded
+        if (!window.kommunicate) {
+            (function (d, m) {
+                var kommunicateSettings = {
+                    appId: "31fd68ece1d068a7f44f69dc536b1fb3a",
+                    popupWidget: true,
+                    automaticChatOpenOnNavigation: true
+                };
+                var s = document.createElement("script");
+                s.type = "text/javascript";
+                s.async = true;
+                s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+                var h = document.getElementsByTagName("head")[0].appendChild(s);
+                window.kommunicate = m;
+                m._globals = kommunicateSettings;
+            })(document, window.kommunicate || {});
+        } else {
+            // If Kommunicate is already loaded, just set globals
+            window.kommunicate._globals = {
+                appId: "31fd68ece1d068a7f44f69dc536b1fb3a",
+                popupWidget: true,
+                automaticChatOpenOnNavigation: true
+            };
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                {/* Your chat widget can be placed here or elsewhere in your component */}
+                <br /> <br /> <br />
+            </div>
+        );
+    }
 }
-render(){
-   return(
-       <div>
-        <br /> <br /> <br />
-     </div>
-   )
-}
-}
+
 export default KommunicateChat;
